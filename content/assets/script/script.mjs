70d74ea7f92b9c62fb37340edebfd28eb879d719
Laurent Christophe <laurent.christophe@sirris.be>
2025-11-06 09:17:40 +0100

    refactor: more modular script

--- content/assets/script/script.mjs
+++ content/assets/script/script.mjs
@@ -1,14 +1,5 @@
 import events from "./events.json" with { type: "json" };
 
-{
-  const links = Array.from(document.querySelectorAll("nav ul a"));
-  for (const link of links) {
-    if (link.href === window.location.href) {
-      link.classList.add("active");
-    }
-  }
-}
-
 const parseDate = (date_string) => {
   const [date, time, zone] = date_string.split(" ");
   return new Date(`${date}T${time}${zone}`);
@@ -30,23 +21,38 @@ const formatDate = (date_string) => {
 
 const today = toMidnightStamp(new Date());
 
-const upcoming = events.filter(
-  (event) => parseDate(event.date).getTime() >= today
-);
-
-if (upcoming.length > 0) {
-  const sidebar = document.getElementById("sidebar");
-  {
-    const title = document.createElement("h3");
-    title.textContent = "Upcoming Events:";
-    sidebar.appendChild(title);
-  }
-  {
-    for (const event of upcoming) {
-      const link = document.createElement("a");
-      link.href = event.url;
-      link.textContent = `\u2022\u00A0\u00A0\u00A0${formatDate(event.date)} - ${event.short_title}`;
-      sidebar.appendChild(link);
+const setActiveNav = (document) => {
+  const links = Array.from(document.querySelectorAll("nav ul a"));
+  for (const link of links) {
+    if (link.href === window.location.href) {
+      link.classList.add("active");
     }
   }
-}
+};
+
+const populateUpcoming = (document) => {
+  const upcoming = events.filter(
+    (event) => parseDate(event.date).getTime() >= today
+  );
+  if (upcoming.length > 0) {
+    const sidebar = document.getElementById("sidebar");
+    {
+      const title = document.createElement("h3");
+      title.textContent = "Upcoming Events:";
+      sidebar.appendChild(title);
+    }
+    {
+      for (const event of upcoming) {
+        const link = document.createElement("a");
+        link.href = event.url;
+        link.textContent = `\u2022\u00A0\u00A0\u00A0${formatDate(event.date)} - ${event.short_title}`;
+        sidebar.appendChild(link);
+      }
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  setActiveNav(document);
+  populateUpcoming(document);
+});
